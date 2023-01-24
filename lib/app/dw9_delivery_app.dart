e31d40985bc63253e7966f83b5eaff0ca266b1ac
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-24 10:52:50 -0300

    configurando tema do app

---
+++ lib/app/dw9_delivery_app.dart
@@ -0,0 +1,17 @@
+import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery app',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
+    );
+  }
+}
