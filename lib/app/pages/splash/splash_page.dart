d4473804f624ee789cb985722244a5364a143cb7
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-24 14:31:53 -0300

    mplementação do componente de botão

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,33 +6,17 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        primaryColor: ColorsApp.i.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorsApp.i.primary,
-          primary: ColorsApp.i.primary,
-          secondary: ColorsApp.i.secondary,
-        ),
-      ),
-      child: Scaffold(
-        appBar: AppBar(title: const Text('splash page')),
-        body: Column(
-          children: [
-            Container(),
-            ElevatedButton(
-              child: const Text('teste'),
-              onPressed: () {},
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('splash page')),
+      body: Column(
+        children: [
+          Container(),
+          DeliveryButton(
+            label: 'Entrar',
+            onPressed: () {},
+          ),
+          TextFormField(),
+        ],
       ),
     );
   }
