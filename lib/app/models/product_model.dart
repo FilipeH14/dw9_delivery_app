af9543f971ba29b97678a448c6ca48de05b9eaa2
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-25 08:34:34 -0300

    tela de splash e model de produto

---
+++ lib/app/models/product_model.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ProductModel {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final String image;
+  
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+}
--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:dw9_delivery_app/app/core/config/env/env.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -8,18 +8,37 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('splash page')),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            width: 200,
-            height: 200,
-            label: Env.i['backend_base_url'] ?? '',
-            onPressed: () {},
-          ),
-          TextFormField(),
-        ],
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: context.percentHeight(.30)),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 50),
+                  DeliveryButton(
+                    width: context.percentWidth(.60),
+                    height: 35,
+                    label: 'Acessar',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
