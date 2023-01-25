ac17ef6923a154bf37fbee85b3fefa05700f1a1e
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-25 09:32:03 -0300

    criando uma binding global de instancia

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,19 +1,23 @@
+import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
 import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dw9DeliveryApp extends StatelessWidget {
   const Dw9DeliveryApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Delivery app',
-      theme: ThemeConfig.theme,
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-      },
+    return ApplicationBinding(
+      child: MaterialApp(
+        title: 'Delivery app',
+        theme: ThemeConfig.theme,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashPage(),
+        },
+      ),
     );
   }
 }
