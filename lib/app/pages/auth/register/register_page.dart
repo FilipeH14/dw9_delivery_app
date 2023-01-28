0b4f682ab335b32a8e3acfa7bbb2197f58ab3e0f
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-28 15:50:25 -0300

    validação nos campos de cadastro

--- lib/app/pages/auth/register/register_page.dart
+++ lib/app/pages/auth/register/register_page.dart
@@ -2,9 +2,29 @@ import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +34,7 @@ class RegisterPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -28,26 +49,48 @@ class RegisterPage extends StatelessWidget {
                 const SizedBox(height: 30),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Nome'),
+                  controller: _nameEC,
+                  validator: Validatorless.required('Nome Obrigatório'),
                 ),
                 const SizedBox(height: 30),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'E-mail'),
+                  controller: _emailEC,
+                  validator: Validatorless.multiple([
+                    Validatorless.required('E-mail obrigatório'),
+                    Validatorless.email('E-mail inválido'),
+                  ]),
                 ),
                 const SizedBox(height: 30),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Senha'),
+                  controller: _passwordEC,
+                  obscureText: true,
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Senha obrigatória'),
+                    Validatorless.min(
+                        6, 'Senha deve conter pelo menos caracteres'),
+                  ]),
                 ),
                 const SizedBox(height: 30),
                 TextFormField(
                   decoration:
                       const InputDecoration(labelText: 'Confirma senha'),
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Senha obrigatória'),
+                    Validatorless.compare(
+                        _passwordEC, 'As senhas devem coincidir'),
+                  ]),
                 ),
                 const SizedBox(height: 30),
                 Center(
                   child: DeliveryButton(
                     width: double.infinity,
                     label: 'CADASTRAR',
-                    onPressed: () {},
+                    onPressed: () {
+                      final valid = _formKey.currentState?.validate() ?? false;
+                      if (valid) {}
+                    },
                   ),
                 ),
               ],
