40dfe643db01106aad13aea625cfabe935d6b848
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-29 18:23:01 -0300

    criando campo de formulário em orderPage

--- lib/app/pages/order/order_page.dart
+++ lib/app/pages/order/order_page.dart
@@ -2,8 +2,10 @@ import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/order/widget/order_field.dart';
 import 'package:dw9_delivery_app/app/pages/order/widget/order_product_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -46,6 +48,44 @@ class OrderPage extends StatelessWidget {
                   const Divider(color: Colors.grey),
                 ],
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total do pedido',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 16),
+                      ),
+                      Text(
+                        r'R$200,00',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(color: Colors.grey),
+                OrderField(
+                  title: 'Endereço de entrega',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite seu endereço',
+                ),
+                const SizedBox(height: 10),
+                OrderField(
+                  title: 'CPF',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite o CPF',
+                ),
+              ],
             ),
           ),
         ],
