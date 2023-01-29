f03139fdb9ffa589c02ee956c79ba46714376b15
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-29 17:35:25 -0300

    componentes de produtos em  roderPage

--- lib/app/pages/home/widgets/shopping_bag_widget.dart
+++ lib/app/pages/home/widgets/shopping_bag_widget.dart
@@ -22,9 +22,12 @@ class ShoppingBagWidget extends StatelessWidget {
     if(!sp.containsKey('accessToken')) {
       // Envio parao login
       final loginResult = await navigator.pushNamed('/auth/login');
+
+      if(loginResult == null || loginResult == false) return;
     }
 
-    // Envio para o order
+    // Envio parra Order
+    await navigator.pushNamed('/order', arguments: bag);
   }
 
   @override
---
+++ lib/app/pages/order/order_page.dart
@@ -0,0 +1,55 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/order/widget/order_product_tile.dart';
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DeliveryAppbar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                children: [
+                  Text(
+                    'Carrinho',
+                    style: context.textStyles.textTitle,
+                  ),
+                  IconButton(
+                    icon: Image.asset('assets/images/trashRegular.png'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 2,
+              (context, index) => Column(
+                children: [
+                  OrderProductTile(
+                    index: index,
+                    orderProduct: OrderProductDto(
+                      amount: 10,
+                      product: ProductModel.fromMap({}),
+                    ),
+                  ),
+                  const Divider(color: Colors.grey),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
