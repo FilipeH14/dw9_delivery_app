b400c95eb63a764dc0872a303b8cee83f46f5fdc
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-29 00:21:47 -0300

    integrando tela de login

--- lib/app/pages/auth/register/register_state.dart
+++ lib/app/pages/auth/register/register_state.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
 import 'package:match/match.dart';
 
 part 'register_state.g.dart';
@@ -6,14 +7,14 @@ part 'register_state.g.dart';
 @match
 enum RegisterStatus { initial, register, success, error }
 
-class RegisterState {
+class RegisterState extends Equatable{
   final RegisterStatus status;
 
-  RegisterState({
+  const RegisterState({
     required this.status,
   });
 
-  RegisterState.initial() : status = RegisterStatus.initial;
+  const RegisterState.initial() : status = RegisterStatus.initial;
 
   RegisterState copyWith({
     RegisterStatus? status,
@@ -22,4 +23,7 @@ class RegisterState {
       status: status ?? this.status,
     );
   }
+  
+  @override
+  List<Object?> get props => [status];
 }
